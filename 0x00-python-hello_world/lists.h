758739809eb629524b33a92b37042d61d016fdfc
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-11-29 07:24:29 -0800

    combo

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,25 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
 
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
-
-#endif /* LISTS_H */
