6f0f178075b78e35610ade030a9ca0f7857a5585
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-02-15 09:38:40 -0800

    queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Script that list all privileges in MYSQL
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- Lists all cities contained in the database
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDER BY c.`id`;
