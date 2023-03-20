2548f2bfa1c7645e930f2fe1ad3176cea876087c
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-03-20 03:38:49 -0700

    states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa takes 3 arguments
+Lists all states from the database hbtn_0e_0_usa.
 """
-
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    # Database credentials
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # connect to MySQL server
-    db = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=dbname,
-            port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT *FROM states ORDER BT id ASC")
-    results = cursor.fetchall()
-    for row in results:
+    import MySQLdb
+    import sys
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
