4337ecc3386a51906c0f092a9c42b5be25bb9adc
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-03-21 07:52:18 -0700

    bee

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
 """
+a script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE"
-        "'{:s}' ORDER BY id ASC".format(sys.argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if i[1] == sys.argv[4]:
-            print(row)
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; match arg given
+    cursor = db.cursor()
+    sql_cmd = "SELECT * \
+                 FROM states \
+                 WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4])
+    cursor.execute(sql_cmd)
+    for state in cursor.fetchall():
+        if state[1] == argv[4]:
+            print(state)
+    cursor.close()
+    db.close()
