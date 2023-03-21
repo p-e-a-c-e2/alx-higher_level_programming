c9542f80fc70c978ddcafab214874cd7d9f1b0f9
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-03-21 07:55:04 -0700

    state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """
- a script that takes in the name of a state as
- an argument and lists all cities of that state,
- using the database hbtn_0e_4_usa
+a script that takes in the name of a state as an argument
+and lists all cities of that state,
+using the database hbtn_0e_4_usa
 """
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
 
-    import MySQLdb
-    import sys
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            state_name=sys.argv[4])
-    cur = db.cursor()
-    query = """SELECT cities.name FROM cities
-           JOIN states ON cities.state_id = states.id
-           WHERE states.name = %s
-           ORDER BY cities.id ASC"""
-    cur.execute(query, (state_name,))
-    results = cur.fetchall()
-    print(", ".join(city[0] for city in results))
+    # create cursor to exec queries using SQL; join two tables for all info
+    cursor = db.cursor()
+    sql_cmd = "SELECT cities.name \
+                 FROM states \
+                 INNER JOIN cities ON states.id = cities.state_id \
+                 WHERE states.name LIKE %s \
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd, (argv[4], ))
 
-    cur.close()
+    # format the printing of cities of same state separated by commas
+    print(', '.join(["{:s}".format(city[0]) for city in cursor.fetchall()]))
+
+    cursor.close()
     db.close()
-
